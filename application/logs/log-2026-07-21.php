602e9bc9e41ce1ff8c49ad41b63891802f3ebbe9
arslankhan0123 <arslan.devsspace@gmail.com>
2026-07-21 06:18:06 -0700

    merge development

--- application/logs/log-2026-07-21.php
+++ application/logs/log-2026-07-21.php
@@ -4,3 +4,5 @@ ERROR - 2026-07-21 00:43:45 --> Could not find the language line "company_addres
 ERROR - 2026-07-21 11:14:00 --> 404 Page Not Found: Faviconico/index
 ERROR - 2026-07-21 12:15:53 --> 404 Page Not Found: Faviconico/index
 ERROR - 2026-07-21 12:33:50 --> 404 Page Not Found: Faviconico/index
+ERROR - 2026-07-21 12:36:05 --> 404 Page Not Found: Faviconico/index
+ERROR - 2026-07-21 12:37:44 --> 404 Page Not Found: Faviconico/index

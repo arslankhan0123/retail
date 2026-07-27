d003f3ec4deb9d8cb59f955ceb7f8b205baa8d27
arslankhan0123 <arslan.devsspace@gmail.com>
2026-07-27 10:15:47 -0700

    working on follow unfollow functionality

--- application/logs/log-2026-07-27.php
+++ application/logs/log-2026-07-27.php
@@ -6,3 +6,4 @@ ERROR - 2026-07-27 16:28:08 --> Severity: Notice --> Undefined variable: warehou
 ERROR - 2026-07-27 16:28:27 --> Severity: Notice --> Undefined variable: warehouse_id F:\Projects\retail\application\views\pos.php 264
 ERROR - 2026-07-27 16:29:16 --> Could not find the language line "company_address"
 ERROR - 2026-07-27 12:30:14 --> 404 Page Not Found: Faviconico/index
+ERROR - 2026-07-27 21:02:58 --> Severity: Notice --> Undefined variable: warehouse_id F:\Projects\retail\application\views\pos.php 264
